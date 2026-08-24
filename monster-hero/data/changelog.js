130613bc5dba91725581b9f4e71a347871d5806c
aknkakykhk-maker <aknkakykhk@gmail.com>
2026-08-24 20:56:27 +0900

    feat: Plantの3箇所染色に対応

--- monster-hero/data/changelog.js
+++ monster-hero/data/changelog.js
@@ -1,6 +1,6 @@
 const CHANGELOG = [
   {
-    date: "2026-08-24 20:42",
+    date: "2026-08-24 20:52",
     type: 'update',
     title: '新モンスター「プラント」を追加しました',
     assistantNotice: { id:'update_notice_plant_market_v1', type:'market' },
