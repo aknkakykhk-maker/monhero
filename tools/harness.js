ee859784eb641ff676f5576d1649433939e78b2f
aknkakykhk-maker <aknkakykhk@gmail.com>
2026-09-09 18:02:13 +0900

    test: expose soul fusion inheritance helper

--- tools/harness.js
+++ tools/harness.js
@@ -184,6 +184,7 @@ const EXPORTED_NAMES = [
   'buildAutoRepeatBreakthroughs',
   'buildFusionBreakthroughPlan',
   'buildFusionDiamondSummary',
+  'buildFusionSoulRankInheritancePlan',
   'buildMasuReincarnation',
   'reconcileMasuPoints',
   'totalBreakthroughPoints',
